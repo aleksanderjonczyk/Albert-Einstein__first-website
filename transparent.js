fec5d4cc04226e01971256f2c8181f390ba1af7a
Yakaii <o.jonczyk2@gmail.com>
2019-03-25 18:49:56 +0100

    Poprawa i dodatki

--- transparent.js
+++ transparent.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     $(window).scroll(function() {
-      if($(this).scrollTop() > 450) { 
+      if($(this).scrollTop() > 650) { 
           $('.navbar').addClass('solid');
       } else {
           $('.navbar').removeClass('solid');
